bf7ce38d7ce513adedea2269f5984a4135848f9e
shaun feakes <sf@Rocket.feakes.lan>
2018-09-21 11:38:56 -0500

    .

--- extras/aqua.sh
+++ extras/aqua.sh
@@ -90,7 +90,7 @@ fi
 
 install
 
-echo "Installed "`cat $AQUA/version.h | cut -d '"' -f 2 | tr '\n' ' '`
+echo "\nInstalled "`cat $AQUA/version.h | cut -d '"' -f 2 | tr '\n' ' '`
 
 upgrade_self
 cd $CWD
