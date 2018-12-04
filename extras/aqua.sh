286d05f8d256259fae0fd485698c0fdc9345161a
sfeakes <sfeakes@gmail.com>
2018-12-04 11:40:35 -0600

    .

--- extras/aqua.sh
+++ extras/aqua.sh
@@ -194,7 +194,7 @@ upgrade_self
 echo "Please make sure to read release notes https://github.com/sfeakes/AqualinkD/blob/master/README.md#release"
 
 
-if [ "$0" == "bash"]; then
+if [ "$0" == "bash" ]; then
   # Was probably run from curl
   echo ""
   echo "Source directory $AQUA"
