9cdea56423b508885615a9e73e36030fb933161f
Lee_Ballard <Lee_Ballard@W10DTV1PQ2.mshome.net>
2019-01-17 15:38:58 -0600

    fix #6: filter pump status is going on and off when spa mode is on

--- aqualinkd.c
+++ aqualinkd.c
@@ -437,8 +437,10 @@ void pass_pda_equiptment_status_item(char* msg)
 		  if (strcasecmp(msg, _aqualink_data.aqbuttons[i].pda_label) == 0)
 		  {
 			logMessage(LOG_DEBUG,"*** Found Status for %s = '%.*s'\n", _aqualink_data.aqbuttons[i].pda_label, AQ_MSGLEN, msg);
-			// It's on if it's listed here.
-			_aqualink_data.aqbuttons[i].led->state = ON;
+			// It's on (or delayed) if it's listed here.
+        	if (_aqualink_data.aqbuttons[i].led->state != FLASH) {
+        		_aqualink_data.aqbuttons[i].led->state = ON;
+        	}
 			break;
 		  }
 		}
@@ -476,13 +478,15 @@ bool process_pda_packet(unsigned char* packet, int length)
         init=true;
       }*/
       
-      // If we get a status packet, and we are on the status menu, this is a list of what's on,
-      // so turn everything off, and just turn on items that are listed.
+      // If we get a status packet, and we are on the status menu, this is a list of what's on
+      // or pending so unless flash turn everything off, and just turn on items that are listed.
       // This is the only way to update a device that's been turned off by a real PDA / keypad.
       if (pda_m_type() == PM_EQUIPTMENT_STATUS) {
         //printf("*** SET ALL OFF ****\n");
         for (i = 0; i < TOTAL_BUTTONS; i++) {
-          _aqualink_data.aqbuttons[i].led->state = OFF;
+        	if (_aqualink_data.aqbuttons[i].led->state != FLASH) {
+        		_aqualink_data.aqbuttons[i].led->state = OFF;
+        	}
         }
         for (i = 1; i < PDA_LINES; i++) {
           pass_pda_equiptment_status_item(pda_m_line(i));
@@ -552,13 +556,17 @@ bool process_pda_packet(unsigned char* packet, int length)
         }
       } else if (pda_m_type() == PM_MAIN || pda_m_type() == PM_BUILDING_MAIN) {
         if (stristr(msg, "POOL MODE") != NULL) {
-          set_pda_led(_aqualink_data.aqbuttons[PUMP_INDEX].led, msg[AQ_MSGLEN-1]);
+        	// If pool mode is on the filter pump is on but if it is off the filter pump might be on if spa mode is on.
+        	if (msg[AQ_MSGLEN-1] == 'N') {
+        		_aqualink_data.aqbuttons[PUMP_INDEX].led->state = ON;
+        	} else if (msg[AQ_MSGLEN-1] == '*') {
+        		_aqualink_data.aqbuttons[PUMP_INDEX].led->state = FLASH;
+        	}
         }else if (stristr(msg, "POOL HEATER") != NULL) {
           set_pda_led(_aqualink_data.aqbuttons[POOL_HEAT_INDEX].led, msg[AQ_MSGLEN-1]);
         }else if (stristr(msg, "SPA MODE") != NULL) {
+        	// when SPA mode is on the filter may be on or pending
           set_pda_led(_aqualink_data.aqbuttons[SPA_INDEX].led, msg[AQ_MSGLEN-1]);
-          if (_aqualink_data.aqbuttons[SPA_INDEX].led == ON)
-            set_pda_led(_aqualink_data.aqbuttons[PUMP_INDEX].led, msg[AQ_MSGLEN-1]);
         }else if (stristr(msg, "SPA HEATER") != NULL) {
           set_pda_led(_aqualink_data.aqbuttons[SPA_HEAT_INDEX].led, msg[AQ_MSGLEN-1]);
         }
